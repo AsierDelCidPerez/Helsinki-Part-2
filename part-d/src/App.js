1e0ed0763281e6bbedc77336e180eb0cbcd685ad
Asier175 <86838694+Asier175@users.noreply.github.com>
2022-04-19 19:56:30 +0200

    Update App.js

--- part-d/src/App.js
+++ part-d/src/App.js
@@ -1,40 +1,72 @@
-import logo from './logo.svg';
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
+const Boton = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>{text}</button>
+  )
+}
 
-const Boton = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+const Calculated = ({valoraciones, calcularTotal}) => {
+  const puntuacion = valoraciones.good - valoraciones.bad;
+  const total = calcularTotal(valoraciones);
 
-const Display = ({left, right}) => <span>Left: {left} & Right: {right}</span>
+  return (
+    <>
+    <tr><td>Average: {puntuacion/total}</td></tr>
+    <tr><td>Positive: {valoraciones.good*100/total}%</td></tr>
+    </>
+  )
+}
+
+const Statistics = ({valoraciones}) => {
+  const calcularTotal = valores => Object.keys(valores)
+                                          .map(elem => valores[elem])
+                                          .reduce((prev, curr) => prev+curr);
+  if(valoraciones.good+valoraciones.bad+valoraciones.neutral === 0){
+    return <p>No feedback given</p>
+  }
+  return (
+    <>
+    <table>
+      <tbody>
+        <tr><td>Good: {valoraciones.good}</td></tr>
+        <tr><td>Neutral: {valoraciones.neutral}</td></tr>
+        <tr><td>Bad: {valoraciones.bad}</td></tr>
+        <tr><td>All: {calcularTotal(valoraciones)}</td></tr>
+        <Calculated valoraciones={valoraciones} calcularTotal={calcularTotal}/>
+      </tbody>
+    </table>
+    </>
+  );
+}
 
 const App = () => {
-  const [clicks, setClicks] = useState({
-    left : 0, right: 0
-  });
-  const [registroClicks, setRegistroClicks] = useState([]);
+  // save clicks of each button to its own state
+  const [valoraciones, setValoraciones] = useState({
+    good : 0, neutral: 0, bad: 0
+  })
 
-  const increasedLeft = () => setClicks({...clicks, left : clicks.left+1});
-
-  const increasedRight = () => setClicks({...clicks, right : clicks.right+1});
-
-  const updateReport = report => setRegistroClicks(registroClicks.concat(report));
-
-  const clickLeft = () => {
-    increasedLeft();
-    updateReport("L");
+  const getIncrement = value => {
+    if(value === 1){
+      return () => setValoraciones({...valoraciones, good: valoraciones.good+1});
+    }else if(value === 0){
+      return () => setValoraciones({...valoraciones, neutral: valoraciones.neutral+1});
+    }else{
+      return () => setValoraciones({...valoraciones, bad: valoraciones.bad+1});
+    }
   }
 
-  const clickRight = () => {
-    increasedRight();
-    updateReport("R");
-  }
-  
+
+
   return (
     <div align="center">
-      <Display left={clicks.left} right={clicks.right}/><br/>
-      <span>Registro: {registroClicks.join(", ")}</span><br/>
-      <Boton handleClick={clickLeft} text="<-"/><br/>
-      <Boton handleClick={clickRight} text="->"/>
+      <h1>Give feedback</h1>
+      <Boton handleClick={getIncrement(1)} text="Good"/>
+      <Boton handleClick={getIncrement(0)} text="Neutral"/>
+      <Boton handleClick={getIncrement(-1)} text="Bad"/>
+      <h3>Statistics</h3>
+      <Statistics valoraciones={valoraciones}/>
     </div>
   )
 }
